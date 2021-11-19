1343e9dbdbcea913169ca045077b3b5ad2a810be
Yury Rivera <yrivera@applaudostudios.com>
2021-11-19 01:48:42 -0600

    :wrench: agregando ts config pliguin

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+export const Header = () => {
+
+  return (
+    <header className="head-random">
+      <h1>Unix</h1>
+      <h2>The Operating system</h2>
+    </header>
+  );
+};
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-declare module 'donatello-svg' {
+declare module 'nice-waves' {
   let lib: any;
   export = lib;
 }
