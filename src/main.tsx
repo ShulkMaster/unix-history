2d3e997a1930195ba2e4a2b0a42262ed896b147a
Yury Rivera <yrivera@applaudostudios.com>
2021-11-19 01:19:29 -0600

    adding TS modules

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Header} from './header/Header';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header/>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module 'donatello-svg' {
+  let lib: any;
+  export = lib;
+}
+
+declare module 'svg-inline-react' {
+  let lib: any;
+  export = lib;
+}
