2d90f29e321f0d8564ef1860e5f346a7694cfac0
Yury Rivera <yrivera@applaudostudios.com>
2021-11-21 16:28:07 -0600

    cambiando ruta de un import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import {Boot} from './components/boot/Boot';
-import {Header} from './components/header/Header';
+import {Boot} from 'components/boot/Boot';
+import {Header} from 'components/header/Header';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
